511c3be6df58930e1579e4ab7cda2afd907e0fb7
Simon <simon@dancingcloudservices.com>
2023-09-12 13:18:12 -0600

    Initial

---
+++ src/main/java/verify/Verify.java
@@ -0,0 +1,8 @@
+package verify;
+
+sealed interface X {}
+public final class Verify implements X {
+  public static void main(String[] args) {
+    System.out.println("Hello Java 17 World!");
+  }
+}
